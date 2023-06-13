45ae9c437c923c434d73a22187a56921237bbe1b
Zach <zacharymccormick755@gmail.com>
2023-06-12 22:01:02 -0600

    load_knowledge_base -> load_db

--- spdb.py
+++ spdb.py
@@ -19,7 +19,7 @@ class spDB:
         self.max_memory_usage = max_memory_usage
         
         # Set the save path to the current directory if it is not specified
-        if self.save_path is None:
+        if save_path is None:
             self.save_path = os.path.join(os.getcwd(), '.spdb')
         else:
             self.save_path = save_path
@@ -32,20 +32,29 @@ class spDB:
     @property
     def vector_dimension(self):
         return self._vector_dimension
+    
+    def add(self, vectors: np.ndarray, text: list) -> None:
+        # add vector to faiss index
 
-    def save(self) -> None:
-        # Save the faiss index to a tmp variable, then set it to None so it doesn't get pickled
-        tmp = self.faiss_index
+        # Validate the inputs
+        is_valid, reason = input_validation.validate_add(
+            vectors, text, self.vector_dimension)
+        if not is_valid:
+            raise ValueError(reason)
+
+        ids = utils.create_faiss_index_ids(self.max_id, vectors.shape[0])
+        self.max_id = ids[-1]
+
+        lmdb_utils.add_vectors_to_lmdb(self.save_path, self.name, vectors, ids)
+        lmdb_utils.add_text_to_lmdb(self.save_path, self.name, text, ids)
+
+        # If the index is not trained, don't add the vectors to the index
         if self.faiss_index is not None:
-            faiss.write_index(self.faiss_index, os.path.join(self.save_path, f'{self.name}.index'))
-            self.faiss_index = None
+            # TODO: transform vectors if necessary
+            self.faiss_index.add_with_ids(vectors, ids)
 
-        # save object to pickle file
-        with open(os.path.join(self.save_path, f'{self.name}.pickle', 'wb')) as f:
-            pickle.dump(self, f)
-
-        # Reset the faiss index
-        self.faiss_index = tmp
+        self._vector_dimension = vectors.shape[1]
+        self.save()
 
     def train(self, use_two_level_clustering: bool = None, pca_dimension: int = None, opq_dimension: int = None, compressed_vector_bytes: int = None, omit_opq: bool = False) -> None:
 
@@ -85,53 +94,6 @@ class spDB:
 
         self.save()
 
-    def add(self, vectors: np.ndarray, text: list) -> None:
-        # add vector to faiss index
-
-        # Validate the inputs
-        is_valid, reason = input_validation.validate_add(
-            vectors, text, self.vector_dimension)
-        if not is_valid:
-            raise ValueError(reason)
-
-        ids = utils.create_faiss_index_ids(self.max_id, vectors.shape[0])
-        self.max_id = ids[-1]
-
-        lmdb_utils.add_vectors_to_lmdb(self.save_path, self.name, vectors, ids)
-        lmdb_utils.add_text_to_lmdb(self.save_path, self.name, text, ids)
-
-        # If the index is not trained, don't add the vectors to the index
-        if self.faiss_index is not None:
-            # TODO: transform vectors if necessary
-            self.faiss_index.add_with_ids(vectors, ids)
-
-        self._vector_dimension = vectors.shape[1]
-        self.save()
-
-    def remove(self, vector_ids: np.ndarray) -> None:
-        # vector_ids can be a list or a 1D numpy array. If it is a list, it will be converted
-        # to a numpy array because faiss.remove_ids requires a numpy array
-
-        if isinstance(vector_ids, list):
-            vector_ids = np.array(vector_ids)
-
-        # Validate the inputs
-        is_valid, reason = input_validation.validate_remove(vector_ids)
-        if not is_valid:
-            raise ValueError(reason)
-
-        # Remove the vectors from the faiss index (has to be done first)
-        self.faiss_index.remove_ids(vector_ids)
-        # Save here in case something fails in the LMDB removal.
-        # We can't have ids in the faiss index that don't exist in the LMDB
-        self.save()
-
-        # remove vectors from LMDB
-        lmdb_utils.remove_vectors_from_lmdb(self.save_path, self.name, vector_ids)
-
-        # remove text from LMDB
-        lmdb_utils.remove_text_from_lmdb(self.save_path, self.name, vector_ids)
-
     def query(self, query_vector: np.ndarray, preliminary_top_k: int = 500, final_top_k: int = 100) -> list:
 
         # query_vector needs to be a 1D array
@@ -156,17 +118,59 @@ class spDB:
             self.save_path, self.name, reranked_I, position_to_id_map)
 
         return reranked_text, reranked_ids
+    
+    def remove(self, vector_ids: np.ndarray) -> None:
+        # vector_ids can be a list or a 1D numpy array. If it is a list, it will be converted
+        # to a numpy array because faiss.remove_ids requires a numpy array
+
+        if isinstance(vector_ids, list):
+            vector_ids = np.array(vector_ids)
+
+        # Validate the inputs
+        is_valid, reason = input_validation.validate_remove(vector_ids)
+        if not is_valid:
+            raise ValueError(reason)
+
+        # Remove the vectors from the faiss index (has to be done first)
+        self.faiss_index.remove_ids(vector_ids)
+        # Save here in case something fails in the LMDB removal.
+        # We can't have ids in the faiss index that don't exist in the LMDB
+        self.save()
+
+        # remove vectors from LMDB
+        lmdb_utils.remove_vectors_from_lmdb(self.save_path, self.name, vector_ids)
+
+        # remove text from LMDB
+        lmdb_utils.remove_text_from_lmdb(self.save_path, self.name, vector_ids)
+    
+    def save(self) -> None:
+        # Save the faiss index to a tmp variable, then set it to None so it doesn't get pickled
+        tmp = self.faiss_index
+        if self.faiss_index is not None:
+            faiss.write_index(self.faiss_index, os.path.join(self.save_path, f'{self.name}.index'))
+            self.faiss_index = None
+
+        # save object to pickle file
+        with open(os.path.join(self.save_path, f'{self.name}.pickle'), 'wb') as f:
+            pickle.dump(self, f)
+
+        # Reset the faiss index
+        self.faiss_index = tmp
 
 
-def load_knowledge_base(name: str, save_path: str) -> spDB:
-    # load KnowledgeBase object from pickle file
-    kb = pickle.load(open(os.path.join(save_path, f'{name}.pickle'), 'rb'))
+def load_db(name: str, save_path: str = None) -> spDB:
+    # use default save path if none is provided
+    if save_path is None:
+        save_path = os.path.join(os.getcwd(), '.spdb')
+
+    # load spDB object from pickle file
+    db = pickle.load(open(os.path.join(save_path, f'{name}.pickle'), 'rb'))
 
     # load faiss index from save path
     try:
-        index = faiss.read_index(os.path.join(kb.save_path, f'{name}.index'))
+        index = faiss.read_index(os.path.join(db.save_path, f'{name}.index'))
     except:
         index = None
-    kb.faiss_index = index
+    db.faiss_index = index
 
-    return kb
+    return db
