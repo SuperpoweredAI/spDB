1cad2f02dd5755bd75680c1eb5bcc3763d06ac12
Justin Clark <justinmauriceclark@gmail.com>
2023-06-15 16:22:30 -0600

    setUp -> setUpClass and tearDown -> tearDownClass
    Add another threading test - remove while adding

--- tests/integration/test_thread_safety.py
+++ tests/integration/test_thread_safety.py
@@ -19,8 +19,8 @@ logger = logging.getLogger(__name__)
 
 
 class TestThreadSafety(unittest.TestCase):
-
-    def setUp(self):
+    @classmethod
+    def setUpClass(self):
         self.db_name = "test_db"
         self.pca_dimension = 256
         self.opq_dimension = 128
@@ -29,14 +29,15 @@ class TestThreadSafety(unittest.TestCase):
         self.query_k = 500
         self.gt_k = 50
         self.db = spDB(self.db_name)
-        self.vectors, self.text, self.queries, self.ground_truths = helpers.faiq_test_data()
+        self.vectors, self.text, self.queries, self.ground_truths = helpers.fiqa_test_data()
         self.db.add(self.vectors, self.text)
         self.db.train(True, self.pca_dimension, self.opq_dimension, self.compressed_vector_bytes, self.omit_opq, num_clusters=20_000)
-
-    def tearDown(self):
+    
+    @classmethod
+    def tearDownClass(self):
         self.db.delete()
 
-    def test__full_eval(self):
+    def test__query_while_adding(self):
         # define helper functions for concurrent testing
         def add_async():
             self.db.add(long_vectors, long_text)
@@ -79,3 +80,51 @@ class TestThreadSafety(unittest.TestCase):
         add_thread.join()
         query_thread.join()
 
+        # make sure we have the proper number of vectors
+        self.assertEqual(self.db.num_vectors, 330_000)
+
+    def test__remove_while_adding(self):
+        # define helper functions for concurrent testing
+        def add_async():
+            self.db.add(long_vectors, long_text)
+
+        def remove_async():
+            logger.info(f'going to start removal request after waiting {expected_add_time} seconds for lmdb adds to complete and long add to start')
+            time.sleep(expected_add_time)  # simulate a slight delay before querying
+            start_time = time.perf_counter()
+            logger.info(f'starting removal request, but expecting to be locked from long faiss.add()')
+            result = self.db.remove(vector_ids=[self.db.max_id])
+            end_time = time.perf_counter()
+            time_taken = end_time - start_time
+
+            logger.info(f'time taken for remove request (initiated after long faiss.add()): {time_taken}')
+            self.assertGreaterEqual(time_taken, expected_add_time, "remove() should take at least as long as add()")
+
+        long_vectors = np.tile(self.vectors, (10, 1))
+        long_text = self.text * 10
+
+        # log expected query time for short query
+        t0 = time.perf_counter()
+        self.db.query(self.queries[0], self.query_k, self.gt_k)
+        t1 = time.perf_counter()
+        expected_query_time = t1 - t0
+        logger.info(f'time to complete short query without blocking thread: {expected_query_time}')
+
+        ##################
+        # THREAD SAFETY TEST
+        ##################
+        # Expected time for add() to complete.
+        expected_add_time = 10
+        # create and start add and query threads
+        add_thread = threading.Thread(target=add_async)
+        query_thread = threading.Thread(target=remove_async)
+        
+        add_thread.start()
+        query_thread.start()
+
+        # wait for both threads to finish execution
+        add_thread.join()
+        query_thread.join()
+
+        # make sure we have the proper number of vectors
+        self.assertEqual(self.db.num_vectors, 629_999)  # 30k + 300k + 300k - 1
