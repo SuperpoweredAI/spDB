08abd1f7c475fad58259331d15865b160d84df18
Justin Clark <justinmauriceclark@gmail.com>
2023-06-15 01:32:59 -0600

    clean up integration tests

--- tests/integration/test_full_eval.py
+++ tests/integration/test_full_eval.py
@@ -1,40 +1,16 @@
 """ Full end to end evaluation of the spDB using the fiqa Beir dataset """
 
-import os
-import sys
-import shutil
 import numpy as np
-import unittest
-import pickle
+import os
 import time
+import unittest
 
-# get the absolute file path of this file
-FILE_PATH = os.path.dirname(os.path.abspath(__file__))
+import helpers
 
 from spdb.spdb import spDB
 from spdb import lmdb_utils
 
 lmdb_utils.MAP_SIZE = 10 * 1024 * 1024 * 1024  # set testing map size to 10 GB
-
-def get_test_data() -> tuple[np.ndarray, list, np.ndarray, np.ndarray]:
-
-    # Get the vectors
-    with open(FILE_PATH + '/../data/fiqa_vectors.pickle', 'rb') as handle:
-        vectors = pickle.load(handle)
-    
-    # Get the query data
-    with open(FILE_PATH + '/../data/fiqa_queries.pickle', 'rb') as handle:
-        queries = pickle.load(handle)
-    
-    # Get the text data
-    with open(FILE_PATH + '/../data/fiqa_text.pickle', 'rb') as handle:
-        text = pickle.load(handle)
-    
-    # Get the ground truths
-    with open(FILE_PATH + '/../data/fiqa_ground_truths.pickle', 'rb') as handle:
-        ground_truths = pickle.load(handle)
-
-    return vectors, text, queries, ground_truths
 
 
 def evaluate(db, queries: np.ndarray, ground_truths: np.ndarray, query_k: int, gt_k: int) -> tuple[float, float, list]:
@@ -56,12 +32,6 @@ def evaluate(db, queries: np.ndarray, ground_truths: np.ndarray, query_k: int, g
     return recall, latency, all_unique_ids
 
 
-def clean_up(db_path: str):
-
-    # Delete the folders
-    shutil.rmtree(db_path)
-
-
 class TestFullSpdbEvaluation(unittest.TestCase):
 
     def setUp(self):
@@ -72,28 +42,23 @@ class TestFullSpdbEvaluation(unittest.TestCase):
         self.omit_opq = True # This speeds up the test with a very small performance hit
         self.query_k = 500
         self.gt_k = 50
+        self.db = spDB(self.db_name)
+        self.vectors, self.text, self.queries, self.ground_truths = helpers.faiq_test_data()
+        self.db.add(self.vectors, self.text)
+        self.db.train(True, self.pca_dimension, self.opq_dimension, self.compressed_vector_bytes, self.omit_opq)
+
+
+    def tearDown(self):
+        self.db.delete()
+
 
     def test__full_eval(self):
-
-        # Get the test data from the pickled files
-        vectors, text, queries, ground_truths = get_test_data()
-
-        # create the database
-        db = spDB(self.db_name)
-
-        # add the data
-        db.add(vectors, text)
-
         # Train the index
-        db.train(True, self.pca_dimension, self.opq_dimension, self.compressed_vector_bytes, self.omit_opq)
 
         # Evaluate the index
-        recall, latency, all_unique_ids = evaluate(db, queries, ground_truths, self.query_k, self.gt_k)
+        recall, latency, all_unique_ids = evaluate(self.db, self.queries, self.ground_truths, self.query_k, self.gt_k)
 
         print ("recall", recall)
-
-        # Delete the index, pickle file and folders
-        clean_up(db.save_path)
 
         # Set the recall cutoff at 0.97 and less than 1
         # If recall is above 1, something went wrong
--- tests/integration/test_thread_safety.py
+++ tests/integration/test_thread_safety.py
@@ -2,55 +2,23 @@
 
 import logging
 import numpy as np
-import os
-import pickle
-import shutil
-import sys
 import threading
 import time
 import unittest
 
+import helpers
 
-logger = logging.getLogger(__name__)
-
-
-# get the absolute file path of this file
-FILE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 from spdb.spdb import spDB
 from spdb import lmdb_utils
 
 lmdb_utils.MAP_SIZE = 10 * 1024 * 1024 * 1024  # set testing map size to 10 GB
 
-def get_test_data() -> tuple[np.ndarray, list, np.ndarray, np.ndarray]:
 
-    # Get the vectors
-    with open(FILE_PATH + '/../data/fiqa_vectors.pickle', 'rb') as handle:
-        vectors = pickle.load(handle)
-    
-    # Get the query data
-    with open(FILE_PATH + '/../data/fiqa_queries.pickle', 'rb') as handle:
-        queries = pickle.load(handle)
-    
-    # Get the text data
-    with open(FILE_PATH + '/../data/fiqa_text.pickle', 'rb') as handle:
-        text = pickle.load(handle)
-    
-    # Get the ground truths
-    with open(FILE_PATH + '/../data/fiqa_ground_truths.pickle', 'rb') as handle:
-        ground_truths = pickle.load(handle)
-
-    return vectors, text, queries, ground_truths
+logger = logging.getLogger(__name__)
 
 
-
-def clean_up(db_path: str):
-
-    # Delete the folders
-    shutil.rmtree(db_path)
-
-
-class TestFullSpdbEvaluation(unittest.TestCase):
+class TestThreadSafety(unittest.TestCase):
 
     def setUp(self):
         self.db_name = "test_db"
@@ -58,40 +26,42 @@ class TestFullSpdbEvaluation(unittest.TestCase):
         self.opq_dimension = 128
         self.compressed_vector_bytes = 32
         self.omit_opq = True # This speeds up the test with a very small performance hit
-        self.query_k = 100
+        self.query_k = 500
         self.gt_k = 50
+        self.db = spDB(self.db_name)
+        self.vectors, self.text, self.queries, self.ground_truths = helpers.faiq_test_data()
+        self.db.add(self.vectors, self.text)
+        self.db.train(True, self.pca_dimension, self.opq_dimension, self.compressed_vector_bytes, self.omit_opq, num_clusters=20_000)
+
+    def tearDown(self):
+        self.db.delete()
 
     def test__full_eval(self):
         # define helper functions for concurrent testing
         def add_async():
-            db.add(long_vectors, long_text)
+            self.db.add(long_vectors, long_text)
 
         def query_async():
             logger.info(f'starting short query after waiting {expected_add_time} seconds for lmdb adds to complete and long add to start')
             time.sleep(expected_add_time)  # simulate a slight delay before querying
             start_time = time.perf_counter()
             logger.info(f'starting short query, but expecting to be locked from long faiss.add()')
-            result = db.query(queries[0], self.query_k, self.gt_k)
+            result = self.db.query(self.queries[0], self.query_k, self.gt_k)
             end_time = time.perf_counter()
             time_taken = end_time - start_time
 
             logger.info(f'time taken for single query (initiated after long faiss.add()): {time_taken}')
             self.assertGreaterEqual(time_taken, expected_add_time, "query() should take at least as long as add()")
 
-        # Get the test data from the pickled files
-        vectors, text, queries, ground_truths = get_test_data()
+        long_vectors = np.tile(self.vectors, (10, 1))
+        long_text = self.text * 10
 
-        long_vectors = np.tile(vectors, (10, 1))
-        long_text = text * 10
-
-        # create the database
-        db = spDB(self.db_name)
-
-        # add the data
-        db.add(vectors, text)
-
-        # Train the index
-        db.train(True, self.pca_dimension, self.opq_dimension, self.compressed_vector_bytes, self.omit_opq, num_clusters=20000)
+        # log expected query time for short query
+        t0 = time.perf_counter()
+        self.db.query(self.queries[0], self.query_k, self.gt_k)
+        t1 = time.perf_counter()
+        expected_query_time = t1 - t0
+        logger.info(f'time to complete short query without blocking thread: {expected_query_time}')
 
         ##################
         # THREAD SAFETY TEST
