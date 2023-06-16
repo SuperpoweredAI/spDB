b5aa3e6139a41f44c27603a5810980462ee3b08b
Nick <nick@levo.financial>
2023-06-16 17:55:05 -0600

    integration test fix so it runs properly in the CI pipeline

--- tests/integration/test_fastapi.py
+++ tests/integration/test_fastapi.py
@@ -4,6 +4,7 @@ import sys
 import os
 import numpy as np
 import time
+import unittest
 
 from helpers import fiqa_test_data
 
@@ -13,39 +14,13 @@ sys.path.append(os.path.join(FILE_PATH, '../../'))
 from api.fastapi import app
 
 
-client = TestClient(app)
-
-db_name = "fiqa_test"
-
-def test_create():
-    response = client.post("/db/create", json={"name": db_name})
-    assert response.status_code == 200
-
-def test_add(vectors, text):
-    batch_size = 100
-    for i in range(0, len(vectors), batch_size):
-        print (i)
-        response = client.post(f"/db/{db_name}/add", json={"vectors": vectors[i:i+batch_size], "text": text[i:i+batch_size]})
-    assert response.status_code == 200
-
-def test_train():
-    response = client.post(f"/db/{db_name}/train", json={"use_two_level_clustering": True, "omit_opq": True})
-    assert response.status_code == 200
-    print (response.text)
-
-def test_query(queries):
-    response = client.post(f"/db/{db_name}/query", json={"query_vector": queries[0].tolist()})
-    print (response.json().keys())
-    assert response.status_code == 200
-
-def test_full_eval(queries: np.ndarray, ground_truths: np.ndarray, query_k: int, gt_k: int):
+def evaluate(client, db_name: str, queries: np.ndarray, ground_truths: np.ndarray, query_k: int, gt_k: int):
     start_time = time.time()
     all_unique_ids = []
     total_sum = 0
     for i in range(queries.shape[0]):
         response = client.post(f"/db/{db_name}/query", json={"query_vector": queries[i].tolist(), "preliminary_top_k": query_k, "final_top_k": gt_k})
         reranked_I = np.array(response.json()['ids'])
-        #_, reranked_I = db.query(queries[i], query_k, gt_k)
         # compute recall
         total_sum += sum([1 for x in reranked_I[:gt_k] if x in ground_truths[i, :gt_k]]) / gt_k
         unique_ids = np.unique(reranked_I)
@@ -54,34 +29,79 @@ def test_full_eval(queries: np.ndarray, ground_truths: np.ndarray, query_k: int,
     end_time = time.time()
     recall = total_sum / ground_truths.shape[0]
     latency = (end_time - start_time) * 1000 / queries.shape[0]
-    print ("recall", recall)
-    print ("latency", latency)
 
-    # Set the recall cutoff at 0.97 and less than 1
-    # If recall is above 1, something went wrong
-    assert recall > 0.97
-    assert recall < 1
-
-    # Make sure latency is less than 25ms (higher cutoff than the other test since there's an http request)
-    assert latency < 25
-
-    # Make sure the length of each unique ID list is equal to the gt_k
-    assert (all([len(x) == gt_k for x in all_unique_ids]))
-
-def test_tear_down():
-    response = client.post(f"/db/{db_name}/delete")
-    assert response.status_code == 200
+    return recall, latency, all_unique_ids
 
 
-if __name__ == "__main__":
+class TestFastAPI(unittest.TestCase):
 
-    vectors, text, queries, ground_truths = fiqa_test_data()
-    vectors = vectors.tolist()
+    def setUp(self):
+        self.client = TestClient(app)
+        self.db_name = "fiqa_test"
+        self.pca_dimension = 256
+        self.opq_dimension = 128
+        self.compressed_vector_bytes = 32
+        self.omit_opq = True
+        self.query_k = 500
+        self.gt_k = 50
 
-    test_create()
-    test_add(vectors, text)
-    test_train()
-    test_query(queries)
-    test_full_eval(queries, ground_truths, 500, 50)
-    test_tear_down()
+        vectors, text, queries, ground_truths = fiqa_test_data()
+        self.vectors = vectors.tolist()
+        self.text = text
+        self.queries = queries
+        self.ground_truths = ground_truths
 
+
+    def test__001_create(self):
+        # Create a new database
+        response = self.client.post("/db/create", json={"name": self.db_name})
+        print (response.text)
+        self.assertTrue(response.status_code == 200)
+
+    def test__002_add(self):
+        # Add vectors to the index
+        batch_size = 100
+        for i in range(0, len(self.vectors), batch_size):
+            print (i)
+            response = self.client.post(f"/db/{self.db_name}/add", json={
+                "vectors": self.vectors[i:i+batch_size],
+                "text": self.text[i:i+batch_size]
+            })
+        self.assertTrue(response.status_code == 200)
+
+    def test__003_train(self):
+        # Train the index, using 2 level clustering
+        response = self.client.post(f"/db/{self.db_name}/train", json={
+            "use_two_level_clustering": True,
+            "pca_dimension": self.pca_dimension,
+            "opq_dimension": self.opq_dimension,
+            "compressed_vector_bytes": self.compressed_vector_bytes,
+            "omit_opq": True
+        })
+        self.assertTrue(response.status_code == 200)
+
+    def test__004_query(self):
+        # Test a single query
+        response = self.client.post(f"/db/{self.db_name}/query", json={"query_vector": self.queries[0].tolist()})
+        self.assertTrue(response.status_code == 200)
+
+    def test__005_full_eval(self):
+        # Run a full evaluation. This will tell us if everything is working properly
+        recall, latency, all_unique_ids = evaluate(
+            self.client, self.db_name, self.queries, self.ground_truths, self.query_k, self.gt_k
+        )
+
+        # Set the recall cutoff at above 0.97 and less than 1
+        # If recall is above 1, something went wrong
+        self.assertGreater(recall, 0.97)
+        self.assertLessEqual(recall, 1)
+
+        # Make sure latency is less than 25ms (higher cutoff than the other test since there's an http request)
+        self.assertLess(latency, 25)
+
+        # Make sure the length of each unique ID list is equal to the gt_k
+        self.assertTrue(all([len(x) == self.gt_k for x in all_unique_ids]))
+
+    def test__006_tear_down(self):
+        response = self.client.post(f"/db/{self.db_name}/delete")
+        assert response.status_code == 200
