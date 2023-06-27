4601b70501c2d0c7d5895f2d9845f1a00698ef9e
Nick <nick@levo.financial>
2023-06-26 20:57:36 -0600

    checking that all of the vectors are normalized instead of just the first since it's quick enough (0.5ms per 100 vectors)

--- spdb/input_validation.py
+++ spdb/input_validation.py
@@ -2,6 +2,7 @@ import numpy as np
 import re
 from . import utils
 
+
 def validate_database_name(name: str) -> tuple[bool, str]:
     # Make sure the DB name is valid. It must be valid for a file name
     name_regex = r'^[a-zA-Z0-9_ -]+$'
@@ -65,9 +66,10 @@ def validate_add(vectors: np.ndarray, text: list, vector_dimension: int, num_vec
         return False, "Number of vectors does not match number of text items. Number of vectors: " + str(vectors.shape[0]) + " Number of text items: " + str(len(text))
     
     # Make sure the vectors are normalized (we're only checking the first vector)
-    if not (0.999 < np.linalg.norm(vectors[0]) < 1.001):
-        return False, "Vector is not normalized"
-    
+    for vector in vectors:
+        if not (0.999 < np.linalg.norm(vector) < 1.001):
+            return False, "Vector is not normalized"
+        
     if is_flat_index:
         # Make sure adding the vectors won't exceed the max memory usage
         new_memory_usage = utils.get_training_memory_usage(vectors.shape[1], num_vectors + vectors.shape[0])
